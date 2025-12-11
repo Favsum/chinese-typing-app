dd2a7eebc0837d98e58e8365698092464489a666
Favsum <132614266+Favsum@users.noreply.github.com>
2025-12-11 14:20:50 +0800

    Change base path for deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // 替换 'REPO_NAME' 为你的 GitHub 仓库名，例如 '/chinese-typing-app/'
   // 如果你是部署到 username.github.io 根目录，则不需要这行或设为 '/'
-  base: '/chinese-typing-app/', 
+  base: '/chinese-typing/', 
 })
